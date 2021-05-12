955bff54fcde2ea821d49e6fe02f5d8705b1db9b
Saurav Rohira <saurav.rohira@engineering.ucla.edu>
2021-05-11 22:01:17 -0700

    created login and signup

---
+++ bb-backend/services/user.services.js
@@ -0,0 +1,21 @@
+const sequelize = require('./db');
+const userModel = sequelize.models.user
+
+exports.createUser = async (username, password, email, primaryComm, primaryDetails) => {
+    let user = {
+        username: username,
+        password: password,
+        email: email,
+        primary_communication_method: primaryComm,
+        primary_communication_details: primaryDetails
+    }
+
+    const addedUser = await userModel.create(user);
+    console.log('User', addedUser.username, 'added successfully!');
+    return addedUser;
+}
+
+exports.fetchUser = async (email) => {
+    const user = await userModel.findOne({where: {email:email}});
+    return user;
+}
