eec022b92b0d151ae0ee7c6a79379a7a52044a70
Saurav Rohira <saurav.rohira@engineering.ucla.edu>
2021-05-14 02:23:50 -0700

    completed create listing page with styling

--- bb-frontend/src/Home.js
+++ bb-frontend/src/Home.js
@@ -4,6 +4,7 @@ import RentalList from './RentalList';
 import { useState } from 'react'
 import Button from '@material-ui/core/Button';
 import ImageUpload from './ImageUpload';
+import CreateListing from './CreateListing';
 // import { useState } from 'react'
 // import Button from '@material-ui/core/Button';
 // import Input from '@material-ui/core/Input';
@@ -18,27 +19,28 @@ function Home() {
   }
 
   return (
-    <div>
-      {!myAccount ? 
-        (
-          <div className="Home">
-          <NavBar />
-          <div className="Home-options">
-              <span className="Button"><Button onClick={changePage}>My Account</Button></span>
-          </div>
-          {/* <FilterBar handleOrderChange={handleOrderChange} handleSortChange={handleSortChange} sort={{forwards, orderBy}} /> */}
-          <RentalList />
-          </div>
-        ) : (
-          <div> 
-            <NavBar/>
-            <div className="Home-options">
-              <span className="Button"><Button onClick={changePage}>Home</Button></span>
-            </div>
-            <Profile/>
-          </div>
-        )}
-    </div>
+    // <div>
+    //   {!myAccount ? 
+    //     (
+    //       <div className="Home">
+    //       <NavBar />
+    //       <div className="Home-options">
+    //           <span className="Button"><Button onClick={changePage}>My Account</Button></span>
+    //       </div>
+    //       {/* <FilterBar handleOrderChange={handleOrderChange} handleSortChange={handleSortChange} sort={{forwards, orderBy}} /> */}
+    //       <RentalList />
+    //       </div>
+    //     ) : (
+    //       <div> 
+    //         <NavBar/>
+    //         <div className="Home-options">
+    //           <span className="Button"><Button onClick={changePage}>Home</Button></span>
+    //         </div>
+    //         <Profile/>
+    //       </div>
+    //     )}
+    // </div>
+    <CreateListing id={7}/>
   );
 }
 
