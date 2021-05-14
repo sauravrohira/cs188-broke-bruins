49704d87c42f747c34f100032757a971f1540043
Saurav Rohira <saurav.rohira@engineering.ucla.edu>
2021-05-13 19:46:02 -0700

    rip this is tough

--- bb-frontend/src/ImageUpload.js
+++ bb-frontend/src/ImageUpload.js
@@ -2,46 +2,51 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class ImageUpload extends Component {
-
-    constructor(props) {
-        super(props);
-
-        this.onFileChange = this.onFileChange.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-
-        this.state = {
-            profileImg: ''
-        }
+    state = {
+        imageUrl: null,
+        imageAlt: null,
     }
 
-    onFileChange(e) {
-        this.setState({ profileImg: e.target.files[0] })
-    }
+    handleImageUpload = () => {
+        const { files } = document.querySelector('input[type="file"]')
+        const formData = new FormData();
+        formData.append('file', files[0]);
+        // replace this with your upload preset name
+        formData.append('upload_preset', 'qy7jp6yj');
+        const options = {
+            method: 'POST',
+            body: formData,
+        };
 
-    onSubmit(e) {
-        e.preventDefault()
-        const formData = new FormData()
-        axios.post("http://localhost:8000/api/image/upload", formData, {
-        }).then(res => {
-            console.log(res)
+        // replace cloudname with your Cloudinary cloud_name
+        return fetch('https://api.Cloudinary.com/v1_1/dwb7bm6hn/image/upload', options)
+            .then(res => res.json())
+            .then(res => {
+                this.setState({
+                    imageUrl: res.secure_url,
+                    imageAlt: `An image of ${res.original_filename}`
+                })
         })
+            .catch(err => console.log(err));
     }
-
-
+    
     render() {
+        const {imageUrl, imageAlt} = this.state
         return (
-            <div className="container">
-                <div className="row">
-                    <form onSubmit={this.onSubmit} encType='multipart/form-data'>
-                        <div >
-                            <input type="file" name='image' onChange={this.onFileChange} />
-                        </div>
-                        <div >
-                            <button className="btn btn-primary" type="submit">Upload</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+        <div>
+            <section className="left-side">
+                <form>
+                    <div className="form-group">
+                        <input type="file"/>
+                    </div>
+                    <button type="button" className="btn" onClick={this.handleImageUpload}>Submit</button>
+                </form>
+            </section>
+            <section className="right-side">
+                <p>The resulting image will be displayed here</p>
+                 {imageUrl && (<img src={imageUrl} alt={imageAlt} className="displayed-image"/>)}
+            </section>
+      </div>
         )
     }
 }
