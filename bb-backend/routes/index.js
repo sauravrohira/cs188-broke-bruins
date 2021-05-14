36ed581324c538fae1170a6ae4d98f7bcc9630ac
Saurav Rohira <saurav.rohira@engineering.ucla.edu>
2021-05-13 18:10:25 -0700

    progress

--- bb-backend/routes/index.js
+++ bb-backend/routes/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
-//include all versions of api:
 router.use("/user", require('./user.routes'));
-router.get('/user/login', async (req, res) => { return res.status(200).send("hello")})
+router.use("/image", require('./image.routes'));
+
 
 module.exports = router;
 
