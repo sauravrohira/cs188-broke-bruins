e81a7da3b76213418f03ec23ac44538d857d0052
Raghav Dhall <raghavdhall@Raghavs-MBP.lan>
2021-05-14 13:40:30 -0700

    NodeMailer config

--- bb-backend/controllers/user.controller.js
+++ bb-backend/controllers/user.controller.js
@@ -20,6 +20,34 @@ exports.signup = async (req,res) => {
         })
     }
 
+    //nodemailer config
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          type: 'OAuth2',
+          user: process.env.MAIL_USERNAME,
+          pass: process.env.MAIL_PASSWORD,
+          clientId: process.env.OAUTH_CLIENTID,
+          clientSecret: process.env.OAUTH_CLIENT_SECRET,
+          refreshToken: process.env.OAUTH_REFRESH_TOKEN
+        }
+    });
+
+    let mailOptions = {
+        from: raghavdhall@gmail.com,
+        to: email,
+        subject: 'Welcome to Broke Bruins!',
+        text: 'Hi, this is your verification email!'
+    };
+
+    transporter.sendMail(mailOptions, function(err, data) {
+        if (err) {
+          console.log("Error " + err);
+        } else {
+          console.log("Email sent successfully");
+        }
+    });
+
     //hash password before storing it
     password = await bcrypt.hash(password, 10);
 
