918da9195d7b51a71f90be7fc77712b50ad883a6
Saumya <saumyadedhia@Saumyas-MacBook-Pro-6.local>
2021-05-13 23:05:58 -0700

    Added missing lines

--- bb-backend/routes/index.js
+++ bb-backend/routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 
 router.use("/user", require('./user.routes'));
+router.use("/offer", require('./offer.routes'));
+router.use("/rental", require('./rental.routes'));
 
 module.exports = router;
 
