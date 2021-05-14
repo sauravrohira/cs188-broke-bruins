9fe7996034d34d4653030ec14af8995667416c97
Raghav Dhall <raghavdhall@Raghavs-MBP.lan>
2021-05-14 15:10:34 -0700

    New refresh token

--- bb-backend/services/emailService.js
+++ bb-backend/services/emailService.js
@@ -9,7 +9,7 @@ nodemailer = require('nodemailer')
           pass: "Shakti@1234",
           clientId: "1047170839854-6os4a3sf98o6olth0n9i5sqe649ihbgk.apps.googleusercontent.com",
           clientSecret: "BxmwuoHQ_mmpyXJheGIDmhAY",
-          refreshToken: "1//04Twag_Afti0eCgYIARAAGAQSNwF-L9IrqFjnYLGFEoOr_PrAU8IhSw_XurtbS3r0SFDw60lacueQrtrl3CPCe1BOUAU91WsSGgM"
+          refreshToken: "1//04l-HuEwKcvOOCgYIARAAGAQSNwF-L9IrdgfGeZ0TZ45qOFh_JmTA19SpZM3eZ-WFhdTohIYpUZmSa6Ofl2LTa1sW8Yb9-vjr3CE"
         }
     });
 
