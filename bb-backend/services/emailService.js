f1e3830b15d66368cd30c5d3b2fc70bc3399a381
Raghav Dhall <raghavdhall@Raghavs-MBP.lan>
2021-05-14 15:07:40 -0700

    Added credentials

--- bb-backend/services/emailService.js
+++ bb-backend/services/emailService.js
@@ -5,11 +5,11 @@ nodemailer = require('nodemailer')
         service: 'gmail',
         auth: {
           type: 'OAuth2',
-          user: process.env.MAIL_USERNAME,
-          pass: process.env.MAIL_PASSWORD,
-          clientId: process.env.OAUTH_CLIENTID,
-          clientSecret: process.env.OAUTH_CLIENT_SECRET,
-          refreshToken: process.env.OAUTH_REFRESH_TOKEN
+          user: "raghavdhall@gmail.com",
+          pass: "Shakti@1234",
+          clientId: "1047170839854-6os4a3sf98o6olth0n9i5sqe649ihbgk.apps.googleusercontent.com",
+          clientSecret: "BxmwuoHQ_mmpyXJheGIDmhAY",
+          refreshToken: "1//04Twag_Afti0eCgYIARAAGAQSNwF-L9IrqFjnYLGFEoOr_PrAU8IhSw_XurtbS3r0SFDw60lacueQrtrl3CPCe1BOUAU91WsSGgM"
         }
     });
 
