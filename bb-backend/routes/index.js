6aa202ce2387b81db2d4b4bde1238f27cae2a8f0
Saurav Rohira <saurav.rohira@engineering.ucla.edu>
2021-05-18 18:48:18 -0700

    set up server-side image uploads

--- bb-backend/routes/index.js
+++ bb-backend/routes/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 router.use("/user", require('./user.routes'));
 router.use("/offer", require('./offer.routes'));
 router.use("/rental", require('./rental.routes'));
+router.use("/image", require('./image.routes'))
 
 module.exports = router;
 
