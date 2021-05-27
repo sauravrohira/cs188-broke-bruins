c3a7da73faa1eaaa4c2c2ae65c62558cc3822b6f
Raghav Dhall <raghavdhall@Raghavs-MBP.lan>
2021-05-27 13:38:53 -0700

    Shifted mail to brokebruins5@gmail.com

--- bb-backend/services/emailService.js
+++ bb-backend/services/emailService.js
@@ -1,16 +1,14 @@
 nodemailer = require('nodemailer')
 
-//nodemailer config
-    let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          type: 'OAuth2',
-          user: "raghavdhall@gmail.com",
-          pass: "Shakti@1234",
-          clientId: "1047170839854-6os4a3sf98o6olth0n9i5sqe649ihbgk.apps.googleusercontent.com",
-          clientSecret: "BxmwuoHQ_mmpyXJheGIDmhAY",
-          refreshToken: "1//04l-HuEwKcvOOCgYIARAAGAQSNwF-L9IrdgfGeZ0TZ45qOFh_JmTA19SpZM3eZ-WFhdTohIYpUZmSa6Ofl2LTa1sW8Yb9-vjr3CE"
-        }
-    });
+var transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure:false,
+  requireTLS:true,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD
+  }
+});
 
 module.exports = transporter;
